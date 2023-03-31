f2d0bfbf7ae84c0c7c8fa46d23ea0f867d7c7c49
jasoonmurraay <98032625+jasoonmurraay@users.noreply.github.com>
2023-03-31 15:47:22 -0400

    Added stylings to single artist page
    made artist page accessible from top artists

--- components/ArtistPage.js
+++ components/ArtistPage.js
@@ -1,49 +1,19 @@
-import { useEffect, useState } from "react";
-import getOneArtist from "@/pages/api/getOneArtist";
+import classes from './ArtistPage.module.css'
 
 const ArtistPage = (props) => {
-  const [isPending, setIsPending] = useState(true);
-  const [artistName, setArtistName] = useState(null);
-  const [followers, setFollowers] = useState(null);
-  const [genres, setGenres] = useState([]);
-  const [images, setImages] = useState([]);
-  const [popularity, setPopularity] = useState(null);
-  const [error, setError] = useState(null);
-  const url = `https://api.spotify.com/v1/artists/${props.id}`;
-  useEffect(() => {
-    async function fetchData() {
-        return await getOneArtist(url, props.token);
-    }
-    fetchData().then((data) => {
-      console.log("Data: ", data);
-      setArtistName(data.name);
-      setFollowers(data.followers.total);
-      setGenres(data.genres);
-      setImages(data.images);
-      setPopularity(data.popularity)
-      setIsPending(false);
-    });
-  }, []);
-
   const renderGenres = () => {
-    return genres.map((genre, index) => {
-      return <li key={index}>{genre}</li>;
+    return props.genres.map((genre, index) => {
+      return <li className={`${classes.genre}`} key={index}>{genre}</li>;
     });
   };
-
   return (
-    <>
-      {!isPending && (
-        <>
-          <h1>{artistName}</h1>
-          <img src={images[0].url} alt="" />
-          <ul>{renderGenres()}</ul>
-          <p>{`${followers} followers`}</p>
-          <p>Popularity rating: {popularity}</p>
-        </>
-      )}
-      {isPending && <h1>Loading</h1>}
-    </>
+    <div className={`${classes.content}`}>
+      <h1>{props.artistName}</h1>
+      <img className={`${classes.img}`} src={props.images[0].url} alt={`A picture of ${props.artistName}`} />
+      <p>{`${props.followers} followers`}</p>
+      <ul className={`${classes.list}`}>{renderGenres()}</ul>
+
+    </div>
   );
 };
 
--- pages/artists/[ArtistId].js
+++ pages/artists/[ArtistId].js
@@ -1,17 +1,60 @@
 import ArtistPage from "@/components/ArtistPage";
 import Navbar from "@/components/Navbar";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { SpotifyContext } from "@/context/spotifyContext";
 import { useRouter } from "next/router";
+import getOneArtist from "../api/getOneArtist";
+import Loading from "@/components/Loading";
 
 const Artist = () => {
   const router = useRouter();
   const { ArtistId } = router.query;
-  const [token, setToken] = useState("");
-  const [loggedIn, setIsLoggedIn] = useState(true)
- 
-  return <>
-  <Navbar isLoggedIn={loggedIn}></Navbar>
-  <ArtistPage id={ArtistId}></ArtistPage>
+  console.log("Artist ID: ", ArtistId)
+  const { spotifyTokenState, updateSpotifyTokenState } = useContext(SpotifyContext)
+  const [isPending, setIsPending] = useState(true);
+  const [artistName, setArtistName] = useState(null);
+  const [followers, setFollowers] = useState(null);
+  const [genres, setGenres] = useState([]);
+  const [images, setImages] = useState([]);
+  const [popularity, setPopularity] = useState(null);
+  const [error, setError] = useState(null);
+  const url = `https://api.spotify.com/v1/artists/${ArtistId}`;
+
+  function setCommas(number) {
+    console.log("Followers: ", number)
+    let numStr = number.toString().split('')
+    for (let i = numStr.length - 3; i > 0; i -= 3) {
+      numStr.splice(i, 0, ',')
+    }
+    return numStr.join('')
+  }
+  useEffect(() => {
+    if (!ArtistId) {
+      return
+    }
+    renderArtist()
+  }, [ArtistId]);
+
+  async function renderArtist() {
+    async function fetchData() {
+      console.log("URL: ", url)
+      return await getOneArtist(url, spotifyTokenState.token, spotifyTokenState.isTokenValid);
+    }
+    return fetchData().then((data) => {
+      console.log("Data: ", data);
+      setArtistName(data.name);
+      setFollowers(setCommas(data.followers.total));
+      setGenres(data.genres);
+      setImages(data.images);
+      setPopularity(data.popularity)
+      setIsPending(false);
+    })
+  }
+
+  return <><Navbar></Navbar>
+    {isPending && <Loading></Loading>}
+    {!isPending && <ArtistPage artistName={artistName} followers={followers} genres={genres} images={images} popularity={popularity}></ArtistPage>}
+
   </>;
 };
 
