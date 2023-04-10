68ab409bc6aa09c211515f7e6b5369b4fae0e01b
jasoonmurraay <98032625+jasoonmurraay@users.noreply.github.com>
2023-04-10 16:30:24 -0400

    Track page stylings
    Added ability to reorder playlists with >100 songs
    
    Introduced functionality for copying existing playlist

--- components/TopTracks.js
+++ components/TopTracks.js
@@ -9,18 +9,20 @@ const TopTracks = (props) => {
                 <li key={track.id} className={`${classes.card} card col-xl-3 col-md-4 col-12 d-flex align-items-center justify-content-center`}>
                     <div className={`${classes.cardBody}`}>
                         <img className={`${classes.img} card-img`} src={track.album.images[0].url} alt="" />
-                        <h2 className="card-title">{track.name}</h2>
-                        <div className={`${classes.artistDiv}`}>
-                            {track.artists.length === 1 ? "Artist: " : "Artists: "}{" "}
-                            <ul className={`${classes.artistUl}`}>
-                                {track.artists.map((artist) => {
-                                    return (
-                                        <a className={`${classes.artists}`} key={artist.id} href={artist.external_urls.spotify}>
-                                            <li className={`${classes.artistName}`}>{artist.name}</li>
-                                        </a>
-                                    );
-                                })}
-                            </ul>
+                        <div className={`${classes.cardText}`}>
+                            <h2 className={`${classes.trackName} card-title`}>{track.name}</h2>
+                            <div className={`${classes.artistDiv}`}>
+                                {track.artists.length === 1 ? "Artist: " : "Artists: "}{" "}
+                                <ul className={`${classes.artistUl}`}>
+                                    {track.artists.map((artist) => {
+                                        return (
+                                            <a className={`${classes.artists}`} key={artist.id} href={artist.external_urls.spotify}>
+                                                <li className={`${classes.artistName}`}>{artist.name}</li>
+                                            </a>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </li>
@@ -29,7 +31,7 @@ const TopTracks = (props) => {
     };
     return (<>
         <div className={`${classes.content} d-flex align-items-center justify-content-center`}>
-            <h1 className='d-flex align-items-center justify-content-center'>Top Tracks</h1>
+            <h1 className={`${classes.header} d-flex align-items-center justify-content-center`}>Top Tracks</h1>
             <ul className={`${classes.list}`}>{renderTracks()}</ul>
             <Link href={'/profile'}>
                 <button className={`${classes.profileButton}`}>Back to profile</button>
--- components/UserPlaylists.js
+++ components/UserPlaylists.js
@@ -1,10 +1,13 @@
 import getUserPlaylists from "@/pages/api/getUserPlaylists";
 import { useEffect, useState, useContext } from "react";
 import { SpotifyContext } from "@/context/spotifyContext";
-import classes from "./ProfilePage.module.css"
+import classes from "./UserPlaylists.module.css"
 import ButtonComponent from "./ButtonComponent";
+import copyPlaylist from "@/pages/api/copyPlaylist";
+
 
 const UserPlaylists = (props) => {
+    const { spotifyTokenState, updateSpotifyToken, updateId } = useContext(SpotifyContext)
     const playlists = props.playlists
     const reorderHandler = (id) => {
         const reorderPlaylist = async (id) => {
@@ -17,27 +20,35 @@ const UserPlaylists = (props) => {
     const renderPlaylists = () => {
         return playlists.map((playlist) => {
             return (
-                <li key={playlist.id}>
-                    <div
-                        className={`${classes.card} card col-xl-3 col-md-4 col-12 d-flex align-items-center justify-content-center`}
-                    >
-                        <img
-                            className={`${classes.img} card-img`}
-                            src={playlist.images[0].url}
-                            alt=""
-                        />
-                        <a href={playlist.external_urls.spotify}>
-                            <h2 className="card-title">{playlist.name}</h2>
+                <div className={`${classes.playlistItem}`}>
+                    <li className={`${classes.card} card col-xl-3 col-md-4 col-12 d-flex align-items-center justify-content-center`} key={playlist.id}>
+                        <a className={`${classes.name}`} href={playlist.external_urls.spotify}>
+                            <div
+                                className={`${classes.cardBody}`}
+                            >
+                                <img
+                                    className={`${classes.img} card-img`}
+                                    src={playlist.images[0].url}
+                                    alt=""
+                                />
+
+                                <h2 className={` card-title`}>{playlist.name}</h2>
+
+                                {playlist.owner.id !== spotifyTokenState.id && <p className={`${classes.creator}`}>{playlist.owner.display_name}</p>}
+
+                            </div>
                         </a>
-                        <ButtonComponent id={playlist.id} onClick={reorderHandler} text='Re-order playlist' />
-                    </div>
-                </li>
+                    </li>
+                    {playlist.owner.id === spotifyTokenState.id && <button className={`${classes.reorderButton}`} id={playlist.id} onClick={reorderHandler} >{`Re-order ${playlist.name}`}</button>}
+                </div>
             );
         });
     };
     return (<>
-        <h1>User Playlists</h1>
-        <ul>{renderPlaylists()}</ul>
+        <div className={`${classes.content}`}>
+            <h1>User Playlists</h1>
+            <ul className={`${classes.list}`}>{renderPlaylists()}</ul>
+        </div>
     </>
     )
 }
